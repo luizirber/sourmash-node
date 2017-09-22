511d40a948e9466cfe548559dc0d06c30e5daf25
Luiz Irber <luiz.irber@gmail.com>
2017-09-22 15:30:13 -0700

    Match murmurhash3 output from sourmash py

--- test/minhash.js
+++ test/minhash.js
@@ -41,3 +41,42 @@ test('MinHash: behavior with maxHash', function (t) {
 
   t.end()
 })
+
+test('MinHash: behavior with scaled (alt to maxHash)', function (t) {
+  var scaled = sourmash.minhash.getScaledForMaxHash(35)
+  var mh = new sourmash.minhash.MinHash(0, 4, {scaled: scaled})
+  t.equals(mh.maxHash, 35)
+
+  mh.addHash(10)
+  mh.addHash(20)
+  mh.addHash(30)
+  t.deepEquals(mh.getMins(), [10, 20, 30])
+  mh.addHash(40)
+  t.deepEquals(mh.getMins(), [10, 20, 30])
+  mh.addHash(36)
+  t.deepEquals(mh.getMins(), [10, 20, 30])
+
+  t.end()
+})
+
+test('MinHash: add short sequence', function (t) {
+  var mh = new sourmash.minhash.MinHash(20, 5)
+  mh.addSequence('GGGG')
+  t.equals(mh.getMins().length, 0)
+
+  t.end()
+})
+
+test('MinHash: add sequence', function (t) {
+  var mh = new sourmash.minhash.MinHash(20, 5)
+  mh.addSequence('GGGGG')
+  mh.addSequence('AAAAA')
+  t.deepEquals(mh.getMins(), [2110480117637990133, 10798773792509008305])
+
+  t.end()
+})
+
+test('revcomp', function (t) {
+  t.equals(sourmash.minhash.revcomp('ACGT'), 'ACGT')
+  t.end()
+})
